924cf6d6fbc9c9f858f8694c765669857374f637
Manoj Mahalingam <manojlds@gmail.com>
2012-11-12 23:37:46 +0530

    Adding the initial DSL idea

---
+++ src/cmd.UnitTests/CmdDSLTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+
+namespace cmd.UnitTests
+{
+    [TestFixture]
+    public class CmdDslTests
+    {
+        private dynamic cmd;
+
+        [SetUp]
+        public void SetUp()
+        {
+            cmd = new Cmd();
+        }
+
+        [Test]
+        public void ShouldBeAbleToCallArbitraryCommandOnCmd()
+        {
+            cmd.Git();
+        }
+
+        [Test]
+        public void ShouldBeAbleToCallArbitrarySubCommandOnCommandRunningOnCmd()
+        {
+            cmd.Git().Clone();
+        }
+
+        [Test]
+        public void ShouldBeAbleToPassArgumentsToACommand()
+        {
+            cmd.Git().Clone("http://github.com/manojlds/cmd");
+        }
+
+        [Test]
+        public void ShouldBeAbleToTriggerComandExecutionWithParanthses()
+        {
+            cmd.Git().Clone("http://github.com/manojlds/cmd")();
+        }
+    }
+}
--- src/cmd/Cmd.cs
+++ src/cmd/Cmd.cs
@@ -1,6 +1,30 @@
-﻿namespace cmd
+﻿using System;
+using System.Dynamic;
+
+namespace cmd
 {
-    public class Cmd
+    public class Cmd : DynamicObject
     {
+        public override bool TryInvokeMember(
+        InvokeMemberBinder binder, object[] args, out object result)
+        {
+            try
+            {
+                result = new Cmd();
+                return true;
+            }
+            catch
+            {
+                result = null;
+                return false;
+            }
+        }
+
+        public override bool TryInvoke(
+              InvokeBinder binder, object[] args, out object result)
+        {
+            result = 1;
+            return true;
+        }
     }
 }
